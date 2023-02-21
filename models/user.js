acecf8902efa44e877ee3d2ac4830725be79a165
Marie P <map40j@gmail.com>
2023-02-20 16:32:45 -0800

    finished the user.js file 1st version

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: trim,
+        },
+        emil: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Email address does not match'], 
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },{
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
