3e13a45c009b473a36e190c255d3f585ee91ed56
Marie P <map40j@gmail.com>
2023-03-01 20:12:14 -0800

    redid the connection.js file to be easier to read and added comments

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,15 @@
+// import the mongoose module
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia', {
-       useNewUrlParser: true,
-       useUnifiedTopology: true
-    });
+// define the options to be passed to the mongoose connect method
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
+// connect to the MongoDB database using the connection string specified in the environment variables
+// if no connection string is found, use the default connection string
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia', options);
+
+// export the database connection object for use in other parts of the application
 module.exports = mongoose.connection;
