9be3ff69cc73ca8dd57dfb6a6f098271b467d7ad
Marie P <map40j@gmail.com>
2023-03-01 22:31:37 -0800

    updated the index.js and added comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,9 @@
+// Creates an Express router and sets up routes for users and thoughts by mounting their respective route handlers
 const router = require('express').Router();
-
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
-router
-  .use('/users', userRoutes)
-  .use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes)
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
