54be05d6f533b2296177a8babf42aba323a41624
Marie P <map40j@gmail.com>
2023-03-01 22:29:00 -0800

    updated the thoughts-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,24 +1,33 @@
 const router = require('express').Router();
 const {
-    getThoughts,
-    getSingleThought,
-    createThought,
-    updateThought,
-    deleteThought,
-    addReaction,
-    removeReaction,
- } = require('../../controllers/thought-controller');
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thought-controller');
 
-//api thoughts
-router.route('/').get(getThoughts).post(createThought);
+// To get a thought (all)
+router.get('/', getThoughts);
 
-// /apithought/:thoughtId
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+// To get a thought by id (single)
+router.get('/:thoughtId', getSingleThought);
 
-// api/thought/:thoughtId/reactions
-router.route('/:thoughtId/reaction').post(addReaction);
+// To create a thought
+router.post('/', createThought);
 
-//api/tjhoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+// To update a thought by id
+router.put('/:thoughtId', updateThought);
+
+// To delete a thought by id
+router.delete('/:thoughtId', deleteThought);
+
+// To post a reaction to a thought
+router.post('/:thoughtId/reaction', addReaction);
+
+// To delete a reaction from a thought
+router.delete('/:thoughtId/reactions/:reactionId', removeReaction);
 
 module.exports = router;
